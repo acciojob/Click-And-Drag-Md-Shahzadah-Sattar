ae8892ae0966e95c3341217cbba2e7312dba3338
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2024-01-07 12:24:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Your code here.
+ const itemsContainer = document.querySelector('.items');
+    let isDragging = false;
+    let startPosition = 0;
+    let scrollLeft = 0;
+
+    itemsContainer.addEventListener('mousedown', (e) => {
+      isDragging = true;
+      startPosition = e.clientX;
+      scrollLeft = itemsContainer.scrollLeft;
+
+      itemsContainer.classList.add('active');
+    });
+
+    itemsContainer.addEventListener('mouseup', () => {
+      isDragging = false;
+      itemsContainer.classList.remove('active');
+    });
+
+    itemsContainer.addEventListener('mouseleave', () => {
+      isDragging = false;
+      itemsContainer.classList.remove('active');
+    });
+
+    itemsContainer.addEventListener('mousemove', (e) => {
+      if (!isDragging) return;
+      
+      const delta = e.clientX - startPosition;
+      itemsContainer.scrollLeft = scrollLeft - delta;
+    });
